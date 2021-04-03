af97c529fda35c7fd306418f42dbacd6947efb7b
Antonio Capone <antonio.capone2010@gmail.com>
2021-04-03 21:40:15 +0200

    # WARNING: head commit changed in the meantime
    First Commit on GitHub

---
+++ src/main/java/it/javaboss/Application.java
@@ -0,0 +1,17 @@
+package it.javaboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Spring Boot main Application
+ *
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer
+{
+    public static void main( String[] args ) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/it/javaboss/web/EmployeeManagedBean.java
@@ -0,0 +1,66 @@
+package it.javaboss.web;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SessionScoped
+@ManagedBean
+public class EmployeeManagedBean implements Serializable {
+
+	private static final long serialVersionUID = -2977688161569868585L;
+	
+	// Bean iniettato da CDI
+	@Autowired
+	EmployeeService employeeService;
+	
+	// Nome del dipendente da ricercare
+	private String employeeName;
+	
+	// Risultato della ricerca
+	private List<Employee> result = null;
+	
+	private ResourceBundle bundle;
+	 
+    public ResourceBundle getBundle() {
+        if (bundle == null) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            bundle = context.getApplication().getResourceBundle(context, "msg");
+        }
+        return bundle;
+    }
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public List<Employee> getResult() {
+		return result;
+	}
+
+	public void setResult(List<Employee> result) {
+		this.result = result;
+	}
+
+	public String search() {
+		if ( employeeName == null || employeeName.isEmpty() ) {
+			result = employeeService.getAllEmployees();
+		} else {
+			result = employeeService.searchEmployeesByName(employeeName);
+		}
+		System.out.println( getBundle().getString( "message.search" ) );
+        
+		return null;
+	}
+
+}
